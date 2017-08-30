71103457faa997f78d6512bbb438e2d45555adb9
Yu Chen <yu.chen@live.ie>
2017-08-31 02:04:50 +0800

    fixed default 1

--- src/directive.ts
+++ src/directive.ts
@@ -4,10 +4,11 @@ import { ElementRef, Directive, Input } from '@angular/core';
   selector: '[ycaRatio]'
 })
 export class Ratio {
-  @Input() ycaRatio: number = 1;
+  @Input() ycaRatio: number;
   constructor(private el: ElementRef) { }
 
   ngOnInit() {
+    this.ycaRatio = this.ycaRatio || 1;
     this.el.nativeElement.style.width = '100%';
     let width = this.el.nativeElement.getBoundingClientRect().width;
     this.el.nativeElement.style.height = width * this.ycaRatio + 'px';
